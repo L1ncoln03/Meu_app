6e2774cf67b5421a2e566cc200e490512aa9747b
Lincoln_Gb <130068127+L1ncoln03@users.noreply.github.com>
2023-11-27 22:11:09 -0300

    Add files via upload

---
+++ src/Ativ06.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Ativ06() {
+    return(
+        <>
+            <h1> Atividade 06</h1>
+            <Link to="/">Retornar para a página inicial</Link>
+        </>
+    )
+}
+export default Ativ06;
--- src/minhasRotas.js
+++ src/minhasRotas.js
@@ -3,6 +3,9 @@ import Home from "./Home";
 import Ativ01 from "./Ativ01";
 import Ativ02 from "./Ativ02";
 import Ativ03 from "./Ativ03";
+import Ativ03 from "./Ativ04";
+import Ativ03 from "./Ativ05";
+import Ativ03 from "./Ativ06";
 
 export default function minhasRotas(){
     return(
@@ -12,6 +15,9 @@ export default function minhasRotas(){
                 <Route path="/ativ01" element={<Ativ01/>}/>
                 <Route path="/ativ02" element={<Ativ02/>}/>
                 <Route path="/ativ03" element={<Ativ03/>}/>
+                <Route path="/ativ04" element={<Ativ03/>}/>
+                <Route path="/ativ05" element={<Ativ03/>}/>
+                <Route path="/ativ06" element={<Ativ03/>}/>
 
 
             </Routes>
