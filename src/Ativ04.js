8a0087c374be7d1883232d0244030cc651f409d9
Lincoln_Gb <130068127+L1ncoln03@users.noreply.github.com>
2023-11-27 22:26:09 -0300

    Update Ativ04.js

--- src/Ativ04.js
+++ src/Ativ04.js
@@ -1,60 +1,11 @@
-import React, { useState } from 'react';
-import './App-calc.css';
+import { Link } from "react-router-dom";
 
-function App() {
-  const [display, setDisplay] = useState('0');
-  const [expression, setExpression] = useState('');
-
-  const handleClick = (value) => {
-    if (value === 'C') {
-      setDisplay('0');
-      setExpression('');
-    } else if (value === '=') {
-      try {
-        setDisplay(eval(expression).toString());
-        setExpression('');
-      } catch (error) {
-        setDisplay('Error');
-        setExpression('');
-      }
-    } else {
-      setExpression((prevExpression) => prevExpression + value);
-      setDisplay((prevDisplay) => (prevDisplay === '0' ? value : prevDisplay + value));
-    }
-  };
-
-  return (
-    <div className="calculator">
-      <div className="display">{display}</div>
-      <div className="buttons">
-        <button onClick={() => handleClick('7')}>7</button>
-        <button onClick={() => handleClick('8')}>8</button>
-        <button onClick={() => handleClick('9')}>9</button>
-        <button onClick={() => handleClick('/')}>/</button>
-
-        <button onClick={() => handleClick('4')}>4</button>
-        <button onClick={() => handleClick('5')}>5</button>
-        <button onClick={() => handleClick('6')}>6</button>
-        <button onClick={() => handleClick('*')}>*</button>
-
-        <button onClick={() => handleClick('1')}>1</button>
-        <button onClick={() => handleClick('2')}>2</button>
-        <button onClick={() => handleClick('3')}>3</button>
-        <button onClick={() => handleClick('-')}>-</button>
-
-        <button onClick={() => handleClick('0')}>0</button>
-        <button onClick={() => handleClick('.')}>.</button>
-        <button onClick={() => handleClick('=')} className="equals">
-          =
-        </button>
-        <button onClick={() => handleClick('+')}>+</button>
-
-        <button onClick={() => handleClick('C')} className="clear">
-          C
-        </button>
-      </div>
-    </div>
-  );
+function Ativ04() {
+    return(
+        <>
+            <h1> Atividade 04</h1>
+            <Link to="/">Retornar para a página inicial</Link>
+        </>
+    )
 }
-
-export default App;
+export default Ativ04;
